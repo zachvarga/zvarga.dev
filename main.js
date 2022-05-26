ed7a9496a1842be4eff85e4c7dad19ce651f0b5a
Zach Varga <me@varga.design>
2022-05-26 14:30:01 -0600

    add hero stuff

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Mobile nav burger
 let burger = document.getElementById('burger'),
 	 nav    = document.getElementById('mod-nav');
 
@@ -10,3 +11,46 @@ $(burger).keyup(function(event){
 		// Close the menu
 		$(burger).closeElement();
 }});
+
+// Darkmode Friendly
+// Check to see if Media-Queries are supported
+if (window.matchMedia) {
+    // Check if the dark-mode Media-Query matches
+    if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+      // Dark
+    } else {
+      // Light
+    }
+  } else {
+    // Default (when Media-Queries are not supported)
+  }
+  // Update Color Scheme
+  function setColorScheme(scheme) {
+    switch(scheme){
+      case 'dark':
+        // Dark
+        break;
+      case 'light':
+        // Light
+        break;
+      default:
+        // Default
+        break;
+    }
+  }
+  
+  function getPreferredColorScheme() {
+    if (window.matchMedia) {
+      if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+        return 'dark';
+      } else {
+        return 'light';
+      }
+    }
+    return 'light';
+  }
+  
+  if(window.matchMedia){
+    var colorSchemeQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    colorSchemeQuery.addEventListener('change', setColorScheme(getPreferredColorScheme()));
+  }
